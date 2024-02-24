16e782ebcc3aa946991ee4dfc44e43002669958c
yersultan056 <230129@astanait.edu.kz>
2024-02-24 23:00:33 +0600

    Create spring project

---
+++ src/main/java/kz/aitu/carsharingass4demo/CarSharingAss4DemoApplication.java
@@ -0,0 +1,13 @@
+package kz.aitu.carsharingass4demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarSharingAss4DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarSharingAss4DemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/aitu/carsharingass4demo/CarSharingAss4DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.aitu.carsharingass4demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSharingAss4DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
