c4cfe5cd76d069a7bc06097d9c7b6eb7722b1920
Zharas <231237@astanait.edu.kz>
2024-02-28 00:43:40 +0600

    Booking done

---
+++ src/main/java/kz/aitu/carsharingass4demo/models/Booking.java
@@ -0,0 +1,31 @@
+package kz.aitu.carsharingass4demo.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "bookings")
+public class Booking {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "car_id", referencedColumnName = "car_id")
+    private Cars car;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date startDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date endDate;
+
+
+}
---
+++ src/main/java/kz/aitu/carsharingass4demo/services/BookingService.java
@@ -0,0 +1,50 @@
+package kz.aitu.carsharingass4demo.services;
+
+import kz.aitu.carsharingass4demo.models.Booking;
+import kz.aitu.carsharingass4demo.models.Cars;
+import kz.aitu.carsharingass4demo.models.User;
+import kz.aitu.carsharingass4demo.repositories.BookingRepositoryInterface;
+import kz.aitu.carsharingass4demo.services.Interfaces.BookingInterface;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class BookingService implements BookingInterface {
+    private final BookingRepositoryInterface repo;
+
+    public BookingService(BookingRepositoryInterface repo) {
+        this.repo = repo;
+    }
+
+    @Override
+    public List<Booking> getAll() {
+        return repo.findAll();
+    }
+
+    @Override
+    public Booking getById(int id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    @Override
+    public Booking createBooking(User user, Cars car, Date startDate, Date endDate) {
+        Booking booking = new Booking();
+        booking.setUser(user);
+        booking.setCar(car);
+        booking.setStartDate(startDate);
+        booking.setEndDate(endDate);
+        return repo.save(booking);
+    }
+
+    @Override
+    public List<Booking> getByUser(User user) {
+        return repo.findByUser(user);
+    }
+
+    @Override
+    public List<Booking> getByCar(Cars car) {
+        return repo.findByCar(car);
+    }
+}
