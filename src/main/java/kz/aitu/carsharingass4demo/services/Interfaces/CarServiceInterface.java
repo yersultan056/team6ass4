c9e448a7d573222cf29adaa483af4e9ff00e5c8b
Rakhman <231355@astanait.edu.kz>
2024-02-27 22:08:38 +0600

    Cars Controller and Service interface

---
+++ src/main/java/kz/aitu/carsharingass4demo/services/Interfaces/CarServiceInterface.java
@@ -0,0 +1,14 @@
+package kz.aitu.carsharingass4demo.services.Interfaces;
+
+import kz.aitu.carsharingass4demo.models.Cars;
+
+import java.util.List;
+
+public interface CarServiceInterface {
+    List<Cars> getAll();
+    Cars getByCarId(int carId);
+    Cars createCar(Cars car);
+    List<Cars> getByCarModel(String model);
+    List<Cars> getByCarBrand(String brand);
+    List<Cars> getByCarClass(String carClass);
+}
