c6f7664e5d9406d7c0b904bcdcd222cadd6ee048
Rakhman <231355@astanait.edu.kz>
2024-03-03 19:33:22 +0600

    coment2

--- src/main/java/kz/aitu/carsharingass4demo/repositories/CarRepositoryInterface.java
+++ src/main/java/kz/aitu/carsharingass4demo/repositories/CarRepositoryInterface.java
@@ -1,13 +1,11 @@
 package kz.aitu.carsharingass4demo.repositories;
 
-
 import kz.aitu.carsharingass4demo.models.Car;
-import kz.aitu.carsharingass4demo.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CarRepositoryInterface extends JpaRepository<Car, Integer> {
+    // Find all cars by their brand
     List<Car> findByBrand(String brand);
-
 }
