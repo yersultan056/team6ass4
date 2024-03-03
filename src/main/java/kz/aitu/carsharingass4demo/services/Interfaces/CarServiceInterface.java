a101e7cd9c1766e6d87e60bf0902b6c1e4846270
Rakhman <231355@astanait.edu.kz>
2024-03-03 19:38:52 +0600

    coment3

--- src/main/java/kz/aitu/carsharingass4demo/services/Interfaces/CarServiceInterface.java
+++ src/main/java/kz/aitu/carsharingass4demo/services/Interfaces/CarServiceInterface.java
@@ -5,13 +5,28 @@ import kz.aitu.carsharingass4demo.models.Car;
 import java.util.List;
 
 public interface CarServiceInterface {
+    // Get all cars
     List<Car> getAll();
+
+    // Get a car by its ID
     Car getById(int id);
+
+    // Create a new car
     Car create(Car car);
+
+    // Get all cars by their brand
     List<Car> getByBrand(String brand);
+
+    // Delete a car by its ID
     String deleteById(int id);
+
+    // Update a car
     Car updateCar(int id, Car car);
+
+    // Calculate the cost of renting a car for a specific period
     long calculateCost(int id, String period, long period_number);
 
+    // Calculate the cost of renting a car from a specific start date to an end date
     long calculateCostFromDate(int id, String period_start, String period_end);
 }
+
