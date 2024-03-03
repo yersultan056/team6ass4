b6f5c3ee38d7f51ebdc51b98683fe1e74f718e8e
Rakhman <231355@astanait.edu.kz>
2024-03-03 19:51:53 +0600

    coment5

--- src/main/java/kz/aitu/carsharingass4demo/CarSharingAss4DemoApplication.java
+++ src/main/java/kz/aitu/carsharingass4demo/CarSharingAss4DemoApplication.java
@@ -3,11 +3,14 @@ package kz.aitu.carsharingass4demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Main Spring Boot application class
 @SpringBootApplication
 public class CarSharingAss4DemoApplication {
 
+    // Main method to run the Spring Boot application
     public static void main(String[] args) {
+        // Launch the application
         SpringApplication.run(CarSharingAss4DemoApplication.class, args);
     }
-
 }
+
